39df5906da3c4bcbb85ce171a7b95b90346d017b
PjMitchell <peterjames.m@gmail.com>
2016-07-11 23:37:41 +0100

    fixed typo in build script

--- Build.ps1
+++ Build.ps1
@@ -60,7 +60,7 @@ Invoke-MSBuild
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
-exec { & dotnet test .\test\ConfigServer.Core.Test -c Release }
+exec { & dotnet test .\test\ConfigServer.Core.Tests -c Release }
 
 exec { & dotnet pack .\src\ConfigServer.Core -c Release -o .\artifacts --version-suffix=$revision }  
 exec { & dotnet pack .\src\ConfigServer.FileProvider -c Release -o .\artifacts --version-suffix=$revision }  
