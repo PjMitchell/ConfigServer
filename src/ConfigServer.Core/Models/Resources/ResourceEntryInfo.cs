9465417d5e290576babdb86fc4994206ed484c39
PjMitchell <peter@hilltop-digital.co.uk>
2017-03-15 22:03:52 +0000

    Added Intitial  ResourceStore interfaces

---
+++ src/ConfigServer.Core/Models/Resources/ResourceEntryInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConfigServer.Core
+{
+    /// <summary>
+    /// Information about a Resource entry
+    /// </summary>
+    public class ResourceEntryInfo
+    {
+        /// <summary>
+        /// Name of Resource Entry
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
