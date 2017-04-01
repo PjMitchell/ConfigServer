4649fbc9a811b1bdfaf7a977c8d22aa06824aaf6
PjMitchell <peterjames.m@gmail.com>
2017-04-01 23:04:15 +0100

    Fixed upload-download of configurationset

--- src/ConfigServer.Core/Models/ConfigFactory.cs
+++ src/ConfigServer.Core/Models/ConfigFactory.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 
 namespace ConfigServer.Core
 {
@@ -16,6 +18,23 @@ namespace ConfigServer.Core
         public static ConfigInstance CreateGenericInstance(Type type, ConfigurationClient client)
         {
             var config = typeof(ConfigInstance<>);
+            Type[] typeArgs = { type };
+            var configType = config.MakeGenericType(typeArgs);
+            var result = (ConfigInstance)Activator.CreateInstance(configType);
+            result.ConfigurationIdentity = new ConfigurationIdentity(client);
+            return result;
+        }
+
+        /// <summary>
+        /// Creates Instance of a generic collection ConfigInstance
+        /// </summary>
+        /// <param name="type">Type for generic ConfigInstance</param>
+        /// <param name="configs">Configurations</param>
+        /// <param name="client">Configuration Client</param>
+        /// <returns></returns>
+        public static ConfigInstance CreateGenericCollectionInstance(Type type, ConfigurationClient client)
+        {
+            var config = typeof(ConfigCollectionInstance<>);
             Type[] typeArgs = { type };
             var configType = config.MakeGenericType(typeArgs);
             var result = (ConfigInstance)Activator.CreateInstance(configType);
