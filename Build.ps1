1ec0bad77133686a6af82f08b7a5e5e35cfba83e
Peter Mitchell <peterjames.m@gmail.com>
2017-04-09 17:19:41 +0100

    Release beta 7 (#115)

--- Build.ps1
+++ Build.ps1
@@ -78,7 +78,7 @@ ExecuteGulpTasks
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10);
-$revision = "beta6-" + $revision;
+$revision = "beta7-" + $revision;
 exec { & dotnet build -c Release}
 
 exec { & dotnet test .\test\ConfigServer.Core.Tests\ConfigServer.Core.Tests.csproj -c Release }
@@ -86,12 +86,13 @@ exec { & dotnet test .\test\ConfigServer.Core.Tests\ConfigServer.Core.Tests.cspr
 exec { & dotnet pack .\src\ConfigServer.Core\ConfigServer.Core.csproj -c Release -o .\artifacts --version-suffix=$revision }  
 exec { & dotnet pack .\src\ConfigServer.Server\ConfigServer.Server.csproj -c Release -o .\artifacts --version-suffix=$revision }  
 exec { & dotnet pack .\src\ConfigServer.Client\ConfigServer.Client.csproj -c Release -o .\artifacts --version-suffix=$revision }
+exec { & dotnet pack .\src\ConfigServer.Client.Builder\ConfigServer.Client.Builder.csproj -c Release -o .\artifacts --version-suffix=$revision }  
 
 exec { & dotnet pack .\src\ConfigProviders\ConfigServer.TextProvider.Core\ConfigServer.TextProvider.Core.csproj -c Release -o .\artifacts --version-suffix=$revision }  
 exec { & dotnet pack .\src\ConfigProviders\ConfigServer.FileProvider\ConfigServer.FileProvider.csproj -c Release -o .\artifacts --version-suffix=$revision }  
 exec { & dotnet pack .\src\ConfigProviders\ConfigServer.InMemoryProvider\ConfigServer.InMemoryProvider.csproj -c Release -o .\artifacts --version-suffix=$revision }
 exec { & dotnet pack .\src\ConfigProviders\ConfigServer.AzureBlobStorageProvider\ConfigServer.AzureBlobStorageProvider.csproj -c Release -o .\artifacts --version-suffix=$revision }  
 exec { & dotnet pack .\src\ConfigProviders\ConfigServer.AzureTableStorageProvider\ConfigServer.AzureTableStorageProvider.csproj -c Release -o .\artifacts --version-suffix=$revision }  
-exec { & dotnet pack .\src\ConfigServer.Client.Builder\ConfigServer.Client.Builder.csproj -c Release -o .\artifacts --version-suffix=$revision }  
+
 
 
