d1c7fdc66238f219f31d5bbfc1eb933050186c0f
Peter Mitchell <peterjames.m@gmail.com>
2017-08-28 18:17:10 +0100

    Asserts Assets (#159)
    * Asserts Assets
    
    * Fix build part one
    
    * Fix build part 2
    
    * Build fix part 3
    
    * Fixed build part 4
    
    * Incremented version number

--- Build.ps1
+++ Build.ps1
@@ -79,6 +79,17 @@ function ExecuteGulpTasks
 	Pop-Location
 }
 
+function AssertPath {
+	[CmdletBinding()]	
+	param(
+        [Parameter(Position=0,Mandatory=1)][string]$path
+    )
+	if( (Test-Path $path) -eq $false) {
+		Write-Error "Asset $path not found";
+		exit 1;
+	}
+}
+
 function CopyItemWithAssert {
 	[CmdletBinding()]
     param(
@@ -86,21 +97,30 @@ function CopyItemWithAssert {
         [Parameter(Position=1,Mandatory=0)][string]$target
     )
 	Copy-Item $path $target
-	if( (Test-Path $path) -eq $false) {
-		Write-Error "Asset $path not found";
-		exit 1;
-	}
+	AssertPath $path
 }
 
-function copyAssets {
-	$assetPath = ./src/ConfigServer.Server\Assets
-	$assetLibPath = ./src/ConfigServer.Server\Assets\lib
-	CopyItemWithAssert .\src\ConfigServer.Gui\wwwroot\Assets\app.js $assetPath
-	CopyItemWithAssert .\src\ConfigServer.Gui\wwwroot\Assets\styles.css $assetPath
-	
-	CopyItemWithAssert .\src\ConfigServer.Gui\wwwroot\Assets\lib\shim.min.js $assetLibPath
-	CopyItemWithAssert .\src\ConfigServer.Gui\wwwroot\Assets\lib\system.js $assetLibPath
-	CopyItemWithAssert .\src\ConfigServer.Gui\wwwroot\Assets\lib\zone.min.js $assetLibPath
+function CopyAssets {
+	$assetPath = '.\src\ConfigServer.Server\Assets'
+	$assetLibPath = '.\src\ConfigServer.Server\Assets\lib'
+	New-Item -Path $assetPath -type directory
+	New-Item -Path $assetLibPath -type directory
+	Write-Host "Copying Assets from ConfigServer.Gui to ConfigServer.Server"
+	CopyItemWithAssert '.\src\ConfigServer.Gui\wwwroot\Assets\app.js' $assetPath
+	CopyItemWithAssert '.\src\ConfigServer.Gui\wwwroot\Assets\styles.css' $assetPath
+	Write-Host "Copying Assets/lib from ConfigServer.Gui to ConfigServer.Server"
+	CopyItemWithAssert '.\src\ConfigServer.Gui\wwwroot\Assets\lib\shim.min.js' $assetLibPath
+	CopyItemWithAssert '.\src\ConfigServer.Gui\wwwroot\Assets\lib\system.js' $assetLibPath
+	CopyItemWithAssert '.\src\ConfigServer.Gui\wwwroot\Assets\lib\zone.min.js' $assetLibPath
+}
+
+function AssertAssets {
+	Write-Host "Checking asset have been generated and copied"
+	AssertPath '.\src\ConfigServer.Server\Assets\app.js'
+	AssertPath '.\src\ConfigServer.Server\Assets\styles.css'	
+	AssertPath '.\src\ConfigServer.Server\Assets\lib\shim.min.js' 
+	AssertPath '.\src\ConfigServer.Server\Assets\lib\system.js' 
+	AssertPath '.\src\ConfigServer.Server\Assets\lib\zone.min.js'
 }
 
 if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
@@ -109,10 +129,12 @@ EnsurePsbuildInstalled
 exec { & dotnet --info }
 exec { & dotnet restore }
 ExecuteGulpTasks
-copyAssets
+CopyAssets
+AssertAssets
+
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10);
-$revision = "beta9-" + $revision;
+$revision = "beta10-" + $revision;
 exec { & dotnet build -c Release}
 
 exec { & dotnet test .\test\ConfigServer.Core.Tests\ConfigServer.Core.Tests.csproj -c Release }
