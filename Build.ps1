443e7aff8142b41aaa78e79327224cd4b3bef756
PjMitchell <peter@hilltop-digital.co.uk>
2017-08-28 16:50:06 +0100

    Fix build part 2

--- Build.ps1
+++ Build.ps1
@@ -101,17 +101,20 @@ function CopyItemWithAssert {
 }
 
 function CopyAssets {
+	Write-Host "Copying Assets from ConfigServer.Gui to ConfigServer.Server"
 	$assetPath = '.\src\ConfigServer.Server\Assets'
 	$assetLibPath = '.\src\ConfigServer.Server\Assets\lib'
+	Write-Host "Copying Assets from ConfigServer.Gui to ConfigServer.Server"
 	CopyItemWithAssert .\src\ConfigServer.Gui\wwwroot\Assets\app.js $assetPath
 	CopyItemWithAssert .\src\ConfigServer.Gui\wwwroot\Assets\styles.css $assetPath
-	
+	Write-Host "Copying Assets/lib from ConfigServer.Gui to ConfigServer.Server"
 	CopyItemWithAssert .\src\ConfigServer.Gui\wwwroot\Assets\lib\shim.min.js $assetLibPath
 	CopyItemWithAssert .\src\ConfigServer.Gui\wwwroot\Assets\lib\system.js $assetLibPath
 	CopyItemWithAssert .\src\ConfigServer.Gui\wwwroot\Assets\lib\zone.min.js $assetLibPath
 }
 
 function AssertAssets {
+	Write-Host "Checking asset have been generated and copied"
 	AssertPath .\src\ConfigServer.Server\Assets\app.js 
 	AssertPath .\src\ConfigServer.Server\Assets\styles.css 	
 	AssertPath .\src\ConfigServer.Server\Assets\lib\shim.min.js 
